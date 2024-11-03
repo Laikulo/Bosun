c7821c4001159d34ef8c865ec33c017fa171ade3
Aaron Haun <aaron@haun.guru>
2024-11-03 17:25:37 +0000

    Initial config structure

---
+++ src/python/bosun/klippy_host.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from os import PathLike
+
+class KlippyHost(object):
+    pass
+
+
+class KlippyHostProcessConfig(object):
+    """
+    Represents the configuration of a klippy host
+    """
+    def __init__(self):
+        self.config_path: Optional[PathLike] = None
+        self.input_tty_path: Optional[PathLike] = None
+        self.api_socket_path: Optional[PathLike] = None
+        self.log_path: Optional[PathLike] = None
+        self.verbose: bool = False
+
+        self.extra_args: List[str] = []
+
+    def validate(self):
+        if not config_path:
+            raise ValueError("Printer configuration path was unspecified")
+
+    def startup_args(self):
+        self.validate()
+        startup_args = []
+
+        if self.input_tty_path:
+            startup_args += ['-I', input_tty_path]
+
+        if self.api_socket_path:
+            startup_args += ['-a', api_socket_path]
+
+        if self.log_path:
+            startup_args += ['-l', log_path]
+
+        if self.verbose:
+            startup_args.append('-v')
+
+        if self.config_path:
+            startup_args.append(self.config_path)
+
+        if self.extra_args:
+            startup.args += extra_args
+        
+        return startup_args
