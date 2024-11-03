673a3223f089f728aa3344910da40d8658e6923e
Aaron Haun <aaron@haun.guru>
2024-11-03 13:30:17 -0500

    foo

--- src/python/bosun/__init__.py
+++ src/python/bosun/__init__.py
@@ -2,20 +2,21 @@ from typing import Dict, List, Any, Callable
 
 class Bosun(object):
     def __init__(self):
-        self._hooks = Dict[str,List[Any]]
+        self._hooks: Dict[str,List[Any]] = {}
 
-    def entrypoint():
+    def entrypoint(self):
         # Locate components, and register their hooks
+        from .klippy_host import KlippyHost
+        KlippyHost().setup_env()
+        self._process_hooks('bosun:system_startup')
 
-        _process_hooks('bosun:system_startup')
-
-    def _register_hook(hook_type: str, hook: Callable):
-        if not self._hooks[hook_type]:
+    def _register_hook(self, hook_type: str, hook: Callable):
+        if not hook_type in  self._hooks:
             self._hooks[hook_type] = []
         self._hooks[hook_type].append(hook)
 
-    def _process_hooks(hook_type: str, args: Dict[str, Any] = []):
-        if self._hooks[hook_type]:
+    def _process_hooks(self, hook_type: str, args: Dict[str, Any] = []):
+        if hook_type in self._hooks:
             for hook in self._hooks[hook_type]:
                 hook(**args)
 
@@ -23,8 +24,3 @@ class Bosun(object):
     def run(cls):
         instance = cls()
         return instance.entrypoint()
-
-
-
-if __name__ == "__main__":
-    Bosun.run()
---
+++ src/python/bosun/__main__.py
@@ -0,0 +1,3 @@
+from . import Bosun
+
+Bosun.run()
